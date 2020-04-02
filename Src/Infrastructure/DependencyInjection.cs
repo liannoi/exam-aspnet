0872a9ddb1d23ab4d28f5733a7f91a2e42e87a39
Maksym Liannoi <51001083+liannoi@users.noreply.github.com>
2020-04-02 17:19:37 +0300

    Described an infrastructure layer

---
+++ Src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Exam.Application.Common.Interfaces;
+using Exam.Domain.Entities;
+using Exam.Infrastructure.MockReaders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Exam.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection self)
+        {
+            self.AddTransient<IJsonMocksReader<Film>, JsonMocksReader<Film>>();
+
+            return self;
+        }
+    }
+}
---
+++ Src/Infrastructure/MockReaders/JsonMocksReader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Exam.Application.Common.Interfaces;
+using Newtonsoft.Json;
+
+namespace Exam.Infrastructure.MockReaders
+{
+    public class JsonMocksReader<TEntity> : IJsonMocksReader<TEntity> where TEntity : class, new()
+    {
+        public async Task<IEnumerable<TEntity>> ReadAsync(string filePath, CancellationToken cancellationToken)
+        {
+            return await await Task.Factory.StartNew(
+                async () => JsonConvert.DeserializeObject<IEnumerable<TEntity>>(
+                    await File.ReadAllTextAsync(filePath, cancellationToken)), cancellationToken);
+        }
+    }
+}
